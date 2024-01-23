37bde91ca817852fc743174e69e242c55f01a7c1
Artem Tkachev <tkachevforios@icloud.com>
2024-01-23 21:55:52 +0300

    ListVC: Diffable data source setting

--- HiChat/Controllers/ListViewController.swift
+++ HiChat/Controllers/ListViewController.swift
@@ -7,9 +7,36 @@
 
 import UIKit
 
+struct HChat: Hashable {
+    var userName: String
+    var userImage: UIImage
+    var lastMassage: String
+    var id = UUID()
+    
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id)
+    }
+    
+    static func == (lhs: HChat, rhs: HChat) -> Bool {
+        return lhs.id == rhs.id
+    }
+}
+
 class ListViewController: UIViewController {
     
+    let activeChats: [HChat] = [
+        HChat(userName: "Bob", userImage: UIImage(named: "Person1")!, lastMassage: "How?"),
+        HChat(userName: "Tom", userImage: UIImage(named: "Person2")!, lastMassage: "How are you?"),
+        HChat(userName: "Ron", userImage: UIImage(named: "Person3")!, lastMassage: "What?"),
+        HChat(userName: "Mat", userImage: UIImage(named: "Person4")!, lastMassage: "Hey you?")
+    ]
+    
+    enum Section: Int, CaseIterable {
+        case activeChats
+    }
+    
     var collectionView: UICollectionView!
+    var dataSource: UICollectionViewDiffableDataSource<Section, HChat>?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -17,6 +44,8 @@ class ListViewController: UIViewController {
         view.backgroundColor = .systemMint
         setupCollectionView()
         setupSearchBar()
+        createDataSource()
+        reloadData()
     }
     
     private func setupSearchBar() {
@@ -38,8 +67,30 @@ class ListViewController: UIViewController {
         
         collectionView.register(UICollectionViewCell.self, forCellWithReuseIdentifier: "cellid")
         
-        collectionView.delegate = self
-        collectionView.dataSource = self
+//        collectionView.delegate = self
+//        collectionView.dataSource = self
+    }
+    
+    private func createDataSource() {
+        dataSource = UICollectionViewDiffableDataSource<Section, HChat>(collectionView: collectionView, cellProvider: { collectionView, indexPath, chat in
+            guard let section = Section(rawValue: indexPath.section) else {
+                fatalError("Unknown section kind")
+            }
+            
+            switch section {
+            case .activeChats:
+                let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+                cell.backgroundColor = .systemPink
+                return cell
+            }
+        })
+    }
+    
+    private func reloadData() {
+        var snapshot = NSDiffableDataSourceSnapshot<Section, HChat>()
+        snapshot.appendSections([.activeChats])
+        snapshot.appendItems(activeChats, toSection: .activeChats)
+        dataSource?.apply(snapshot, animatingDifferences: true)
     }
     
     private func createCompositionalLayout() -> UICollectionViewLayout {
@@ -67,19 +118,19 @@ extension ListViewController: UISearchBarDelegate {
 }
 
 // MARK: - UICollectionViewDelegate, UICollectionViewDataSource
-extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
-    
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        5
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
-        cell.backgroundColor = .systemBlue
-        cell.layer.borderWidth = 1
-        return cell
-    }
-}
+//extension ListViewController: UICollectionViewDelegate, UICollectionViewDataSource {
+//    
+//    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+//        5
+//    }
+//    
+//    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+//        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "cellid", for: indexPath)
+//        cell.backgroundColor = .systemBlue
+//        cell.layer.borderWidth = 1
+//        return cell
+//    }
+//}
 
 //extension ListViewController: UICollectionViewDelegateFlowLayout {
 //    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
